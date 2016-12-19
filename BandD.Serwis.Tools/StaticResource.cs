410bc7a5eb9d3f63c8f153446db5d2a9ad5b428a
Newer91 <lisu1991@gmail.com>
2016-12-19 15:20:52 +0100

    Zmiana w FTP

--- BandD.Serwis.Tools/StaticResource.cs
+++ BandD.Serwis.Tools/StaticResource.cs
@@ -8,15 +8,6 @@ namespace BandD.Serwis.Tools
 {
     public static class StaticResource
     {
-        public readonly static string ftpUserName = "servis.bartoszlisowski1-admin";
-        public readonly static string ftpPassword = "StDP8WK1qa";
-
-        /// <summary>
-        /// Adresy do poszczególnych katalogów FTP
-        /// </summary>
-        public readonly static string ftpAddresConfig = @"ftp://213.108.56.205/config/";
-        public readonly static string ftpAddresConfigDev = @"ftp://213.108.56.205/config/devs";
-        public readonly static string ftpAddresLicence = @"ftp://213.108.56.205/licence/";
 
     }
 }
